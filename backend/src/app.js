c2efe2e9eb0357f6ba060847a03e02ad464c7c05
tyagiastha <tyagi655@gmail.com>
2022-01-06 23:06:18 +0530

    Form validation and password encryption

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,13 +11,25 @@ app.get("/", (req, res) => {
 
 app.post("/register", async (req, res) => {
     try {
-        const { firstname, lastname } = req.body;
-        const newUser = new User({
-            firstname: firstname,
-            lastname: lastname
-        })
-        await newUser.save();
-        res.status(201).send(`New user added`);
+        var { password, confirm_password } = req.body;
+        if (password === confirm_password) {
+            const { firstname, lastname, email, phone, username } = req.body;
+            const newUser = new User({
+                firstname: firstname,
+                lastname: lastname,
+                email: email,
+                phone: phone,
+                username: username,
+                password: password,
+                confirm_password: confirm_password
+            })
+            await newUser.save();
+            res.status(201).send(`New user added`);
+        }
+        else {
+            console.log(`Passwords do not match`);
+            res.status(401).send(`Passwords do not match`);
+        }
     }
     catch (err) {
         res.status(400).send(`${err}`);
--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -1,11 +1,43 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 const userSchema = new mongoose.Schema({
     firstname: {
-        type: String
+        type: String,
+        validate: /^[a-z]+$/,
+        required: true
     },
     lastname: {
-        type: String
+        type: String,
+        validate: /^[a-z]+$/
+    },
+    email: {
+        type: String,
+        validate: /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/,
+        required: true
+    },
+    phone: {
+        type: Number,
+        validate: /^([6-9][\d]{9})$/
+    },
+    username: {
+        type: String,
+        validate: /^[\w]+$/,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+        // , validate: /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/
+    },
+    isActive: {
+        type: Boolean,
+        default: false
     }
 })
+
+userSchema.pre("save", async function () {
+    this.password = await bcrypt.hash(this.password, 10);
+})
+
 const User = new mongoose.model("user", userSchema);
 module.exports = User;
